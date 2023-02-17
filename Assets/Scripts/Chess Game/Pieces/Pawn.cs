c8dc17fb050e229b2a5e19b7549c93ec44a00c3e
Ka Yi Liu <kaliu@tcd.ie>
2023-02-17 20:19:19 +0000

    Pawn bugs fixing

--- Assets/Scripts/Chess Game/Pieces/Pawn.cs
+++ Assets/Scripts/Chess Game/Pieces/Pawn.cs
@@ -12,8 +12,22 @@ public class Pawn : Piece
 
     public override void MovePiece(Vector2Int coords)
     {
-        this.occupiedSquare = coords;
-        transform.position = this.board.CalculatePositionFromCoords(coords);
+        if (this.team == TeamColor.White & ((this.occupiedSquare.y != 1 & coords.x - this.occupiedSquare.x == 0 & coords.y - this.occupiedSquare.y == 1) |
+            (this.occupiedSquare.y == 1 & coords.x - this.occupiedSquare.x == 0 & coords.y - this.occupiedSquare.y <= 2 & coords.y - this.occupiedSquare.y >= 1)))
+        {
+            this.occupiedSquare = coords;
+            transform.position = this.board.CalculatePositionFromCoords(coords);
+        }
+        else if (this.team == TeamColor.Black & ((this.occupiedSquare.y != 6 & coords.x - this.occupiedSquare.x == 0 & this.occupiedSquare.y - coords.y == 1) |
+            (this.occupiedSquare.y == 6 & coords.x - this.occupiedSquare.x == 0 & this.occupiedSquare.y - coords.y <= 2 & this.occupiedSquare.y - coords.y >= 1)))
+        {
+            this.occupiedSquare = coords;
+            transform.position = this.board.CalculatePositionFromCoords(coords);
+        }
+        else
+        {
+            transform.position = this.board.CalculatePositionFromCoords(this.occupiedSquare);
+        }
     }
 
 }
