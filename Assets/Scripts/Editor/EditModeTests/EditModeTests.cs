7e6999e2b12a14de007e749108b5fa2bc2204d26
chloecc10 <carey.chloe101@gmail.com>
2023-04-11 11:10:17 -0700

    added unit testing for edit mode before loading scene

--- Assets/Scripts/Editor/EditModeTests/EditModeTests.cs
+++ Assets/Scripts/Editor/EditModeTests/EditModeTests.cs
@@ -16,7 +16,58 @@ public class EditModeTests
         Assert.IsNotNull(ChessBoard);
         
     }
-  
+
+    [Test]
+    public void noPieceGenerationbeforeLoadSceneQueen()
+    {
+        GameObject Queen = GameObject.Find("Queen");
+        Assert.IsNull(Queen);
+
+    }
+
+    [Test]
+    public void noPieceGenerationbeforeLoadSceneKing()
+    {
+        GameObject King = GameObject.Find("King");
+        Assert.IsNull(King);
+
+    }
+
+    [Test]
+    public void noPieceGenerationbeforeLoadSceneRook()
+    {
+        GameObject Rook = GameObject.Find("Rook");
+        Assert.IsNull(Rook);
+
+    }
+
+    [Test]
+    public void noPieceGenerationbeforeLoadSceneBishop()
+    {
+        GameObject Bishop = GameObject.Find("Bishop");
+        Assert.IsNull(Bishop);
+
+    }
+
+    [Test]
+    public void noPieceGenerationbeforeLoadSceneKnight()
+    {
+        GameObject Knight = GameObject.Find("Knight");
+        Assert.IsNull(Knight);
+
+    }
+
+    [Test]
+    public void noPieceGenerationbeforeLoadScenePawn()
+    {
+        GameObject Pawn = GameObject.Find("Pawn");
+        Assert.IsNull(Pawn);
+
+    }
+
+
+
+
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
     [UnityTest]
