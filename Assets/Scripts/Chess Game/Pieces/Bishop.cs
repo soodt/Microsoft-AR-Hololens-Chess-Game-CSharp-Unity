788ad964dd0918234e768ec66ca4abcb43f97b88
Gerald Moore Paris <geraldmoorebparis@gmail.com>
2023-04-12 09:02:42 +0100

    AddComponents using OnPhotonInstantiate.

--- Assets/Scripts/Chess Game/Pieces/Bishop.cs
+++ Assets/Scripts/Chess Game/Pieces/Bishop.cs
@@ -133,4 +133,6 @@ public class Bishop : Piece
         
         return false;
     }
+
+
 }
---
+++ Assets/Scripts/Multiplayer/AddComponents.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.UI;
+
+public class AddComponents : MonoBehaviour, IPunInstantiateMagicCallback
+{
+    public Board board { protected get; set; }
+    public Vector2Int occupiedSquare { get; set; }
+    public TeamColor team { get; set; }
+    public ChessGameController controller { get; set; }
+    public Piece piece { get; set; }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void OnPhotonInstantiate(PhotonMessageInfo info)
+    {
+        //make each piece interactable with AR
+        this.gameObject.AddComponent<BoxCollider>();
+        this.gameObject.AddComponent<NearInteractionGrabbable>();
+        this.gameObject.AddComponent<ObjectManipulator>();
+        //make each piece have the board anchor as parent.
+        this.gameObject.AddComponent<BoardAnchorAsParent>();
+
+        // add snapping to each piece
+        this.GetComponent<ObjectManipulator>().OnManipulationEnded.AddListener(delegate
+        {
+            float distance = board.squareSize * 4;
+            Vector2Int newCoords = new Vector2Int(-1, -1);
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Vector2Int nextSquare = new Vector2Int(i, j);
+                    float newDistance = Vector3.Distance(this.transform.position, board.CalculatePositionFromCoords(nextSquare));
+                    if (newDistance < distance)
+                    {
+                        distance = newDistance;
+                        newCoords.Set(i, j);
+                    }
+                }
+            }
+            if (distance < board.squareSize * 1.5)
+            {
+                this.piece.MovePiece(newCoords);
+            }
+            else
+            {
+                this.piece.MovePiece(this.occupiedSquare);
+            }
+        }
+        );
+    }
+}
