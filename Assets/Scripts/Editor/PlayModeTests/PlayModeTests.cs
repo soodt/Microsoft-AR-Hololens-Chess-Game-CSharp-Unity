2d7954ac7103eff217247329fef64c851b796d83
Steven <cataluns@tcd.ie>
2023-04-13 20:15:26 +0100

    starting play mode tests

--- Assets/Scripts/Editor/PlayModeTests/PlayModeTests.cs
+++ Assets/Scripts/Editor/PlayModeTests/PlayModeTests.cs
@@ -1,3 +1,5 @@
+#if !WINDOWS_UWP
+
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
@@ -13,25 +15,63 @@ using Microsoft.MixedReality.Toolkit.UI;
 using Microsoft.MixedReality.Toolkit.Tests;
 using Microsoft.MixedReality.Toolkit.Utilities;
 */
-public class PlayModeTests
+//using UnityEngine.InputSystem.XR;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+
+//using Microsoft.MixedReality.Toolkit.UI;
+
+
+namespace Microsoft.MixedReality.Toolkit.Tests
+{
+    public class PlayModeTests
 {
 
     private const string sceneName = "ScaledDownScene";
     Scene mainScene;
 
-    public void Setup() 
+    public void Setup()
     {
-        SceneManager.LoadScene(sceneName);
+        PlayModeTestUtilities.InstallTextMeshProEssentials();
+    }
+    [UnitySetUp]
+    public IEnumerator Init() 
+    {
+        TestUtilities.InitializeMixedRealityToolkit(true);
+        
+        //SceneManager.LoadScene(sceneName);
+        AsyncOperation loadScene = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+        loadScene.allowSceneActivation = true;
+        while(loadScene.isDone != true)
+        {
+            yield return null;
+        }
+        yield return true;
     }
     
     // A Test behaves as an ordinary method
-    [Test]
-    public static void PlayModeTestsSimplePasses()
+    [UnityTest]
+    public static void TestLoadedScene()
     {
         // Use the Assert class to test conditions
         Scene scene = SceneManager.GetActiveScene();
         Assert.IsNotNull(scene);
+        Debug.Log("Scene is loaded");
         //SceneManager.GetActiveScene();
+    }
+
+    [UnityTest]
+    public IEnumerator TestSceneObjects()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        GameObject[] list = scene.GetRootGameObjects();
+        Assert.AreEqual(list.Length, 1);
+
+        GameObject object1 = list[0];
+        Debug.Log(object1.ToString());
+
+        yield return null;
+
     }
 
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
@@ -44,3 +84,6 @@ public class PlayModeTests
         yield return null;
     }
 }
+
+}
+#endif
