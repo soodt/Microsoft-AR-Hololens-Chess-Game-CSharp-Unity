c86c3a257a3dbebc633adaa127983a46033c1733
Steven <cataluns@tcd.ie>
2023-04-13 17:00:09 +0100

    init playmode test scenes

--- Assets/Scripts/Editor/PlayModeTests/PlayModeTests.cs
+++ Assets/Scripts/Editor/PlayModeTests/PlayModeTests.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
-
+//using UnityEditor.Build;
+//using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+/*
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.Tests;
+using Microsoft.MixedReality.Toolkit.Utilities;
+*/
 public class PlayModeTests
 {
+
+    private const string sceneName = "ScaledDownScene";
+    Scene mainScene;
+
+    public void Setup() 
+    {
+        SceneManager.LoadScene(sceneName);
+    }
     
     // A Test behaves as an ordinary method
     [Test]
-    public void PlayModeTestsSimplePasses()
+    public static void PlayModeTestsSimplePasses()
     {
         // Use the Assert class to test conditions
+        Scene scene = SceneManager.GetActiveScene();
+        Assert.IsNotNull(scene);
+        //SceneManager.GetActiveScene();
     }
 
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
